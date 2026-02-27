2c6550e161bfc6dc7894d3d3d71c38588b99d74f
Kadin <kadinoake2005@gmail.com>
2026-02-27 14:03:00 -0800

    Initial Commit

---
+++ WeatherWiz/WeatherWizApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWizApp.swift
+//  WeatherWiz
+//
+//  Created by Kadin Oake on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
