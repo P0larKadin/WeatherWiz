166cc5b42d85dbfcfd259ff51152cc08927e8f6b
Bailey <baileym4556@gmail.com>
2026-03-20 13:52:16 -0700

    - Started creating WeatherConditionView - Added custom message data to WeatherData

--- WeatherWiz/WeatherConditionView.swift
+++ WeatherWiz/WeatherConditionView.swift
@@ -5,4 +5,36 @@
 //  Created by Kadin Oake on 2026-03-20.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherConditionView: View {
+    var body: some View {
+        let condition: String = WeatherData.conditions[Int.random(in: 0..<WeatherData.conditions.count)]
+        var msg: String = ""
+        switch condition {
+        case "Sunny":
+            var msg: String = WeatherData.sunnyMsgs[Int.random(in: 0..<WeatherData.cloudyMsgs.count)]
+        case "Rainy":
+            var msg: String = WeatherData.rainyMsgs[Int.random(in: 0..<WeatherData.cloudyMsgs.count)]
+        case "Cloudy":
+            var msg: String = WeatherData.cloudyMsgs[Int.random(in: 0..<WeatherData.cloudyMsgs.count)]
+        case "Snowy":
+            var msg: String = WeatherData.snowyMsgs[Int.random(in: 0..<WeatherData.cloudyMsgs.count)]
+        default:
+            var msg: String = "Wonderful weather we're having"
+        }
+        
+        VStack {
+            Text(condition == "Cloudy" ? "🌤️" : "☀️").font(Font.largeTitle)
+            Text("Todays Weather is:")
+            Text("\(condition)")
+            Text("\(msg)").font(Font.largeTitle)
+        }
+        
+        
+    }
+}
+
+#Preview {
+    WeatherConditionView()
+}
