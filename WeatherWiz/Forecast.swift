b954946fb6dcb502b6fb7d1f7b7911f3ecf5c305
Bailey <baileym4556@gmail.com>
2026-03-27 14:07:29 -0700

    started forcast View

--- WeatherWiz/Forecast.swift
+++ WeatherWiz/Forecast.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct Forecast: View {
+    let forcastType: String
+    var timeOrDay: String
+    let weatherIMG: String
+    let temperature: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Button("<"){
+                
+            }.font(.largeTitle)
+            VStack{
+                Text(type)
+                    .font(.largeTitle)
+                VStack{
+                    Text(timeOrDay)
+                    Text(weatherIMG).font(.system(size: 50))
+                    Text(temperature)
+                }.background(Color.blue)
+            }
+            Button(">"){
+                
+            }.font(.largeTitle)
+            
+        }
+        
+        
     }
 }
 
 #Preview {
-    Forecast()
+    Forecast(forcastType: "Daily",timeOrDay: "11AM",weatherIMG: "☁️",temperature: "10°C")
 }
